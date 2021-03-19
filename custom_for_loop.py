2feb9e3248810fa9eaee3d9f692d3d6151209c36
Keshav Sompura <keshavsomp@gmail.com>
2021-03-18 18:22:47 -0700

    create custom for loop

---
+++ custom_for_loop.py
@@ -0,0 +1,21 @@
+# Create a custom For Loop
+
+# for num in [1,2,3]
+# for char in "hi there"
+
+def my_for(iterable, func):
+    iterator = iter(iterable)
+    while True:
+        try:
+            i = (next(iterator))
+        except StopIteration:
+            print("End of iterator!")
+            break
+        else:
+            func(i)
+
+def square(x):
+    print(x*x)
+    
+my_for("hello", print) 
+my_for([1,2,3,4,5], square)
