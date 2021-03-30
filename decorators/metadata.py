8133ef115ed953c4e4f5ee908e46bad5af20f568
Keshav Sompura <keshavsomp@gmail.com>
2021-03-29 21:35:56 -0700

    How to preserve the metadata of a function

---
+++ decorators/metadata.py
@@ -0,0 +1,20 @@
+# preserve metadata
+from functools import wraps
+def log_function_data(fn):
+    @wraps(fn) # good developer behavior if releasing code to the public
+    def wrapper(*args, **kwargs):
+        """I AM WRAPPER FUNCTION"""
+        print(f"you are about to call {fn.__name__}")
+        print(f"Here's the documentation: {fn.__doc__}")
+        return fn(*args, **kwargs)
+
+    return wrapper
+
+@log_function_data
+def add(x,y):
+    """Adds two numbers together."""
+    return x + y
+
+print(add.__doc__)
+print(add.__name__)
+help(add)
